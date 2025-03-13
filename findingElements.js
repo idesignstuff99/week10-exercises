cd910f82b12a509d9d2dce31f221798d3e85473d
Tony Morales <75559510+idesignstuff99@users.noreply.github.com>
2025-03-12 21:24:00 -0700

    uploading exercise file

---
+++ findingElements.js
@@ -0,0 +1,15 @@
+// let button = document.getElementById('my-button')
+// console.log(button);
+
+// let buttonsByTag = document.getElementsByTagName('button')
+// console.log(buttonsByTag);
+
+// let buttonByClassName = document.getElementsByClassName('my-class');
+// console.log(buttonByClassName);
+
+// let buttonByCSSSelector = document.querySelectorAll('button.my-class')
+// console.log(buttonByCSSSelector);
+
+let buttonByCSSSelector = document.querySelectorAll('.my-class')
+console.log(buttonByCSSSelector);
+
